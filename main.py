ec3a99826b53d1b730e3afd0be088223bba13d31
NecoMatheus <matheus770@users.noreply.github.com>
2022-07-05 19:44:15 -0300

    correção ideia base da calculadora

--- main.py
+++ main.py
@@ -1,62 +1,58 @@
-from verifications import verification_digit, verification_mat_operation
-import os
+import os, time
+def menunum():
+    return("""
+          1 | 2 | 3 | 4 | 5 |
+          6 | 7 | 8 | 9 | 0 |
+          + | - | / | * | = |
+          """)
+def menuope():
+        return("""
+          + | - | / | * | = |
+          """)
+def showresul(x):
+    str(x)
+    return(f"{x}")
+
 
 n = 0
-while True:
-    n = input("Numero: ")
-    if verification_digit(n) == True:
-        print("Digite o simbolo da operação: ")
-        op = str(input(f"{n} "))
-        if verification_mat_operation(op) == True:
-            n2 = input(f"{n} {op} ")
-            if verification_digit(n2) == True:
-                sm = f"{n}{op}{n2}"
-                sm = eval(sm)
-                print(sm)
-                dj = int(input("1 == continuar conta \n2 == Outra conta \n3 == Sair\n"))
-                if verification_digit(dj) == True:
-                    if(dj == 1 ):
-                        while True:
-                            print("Digite o simbolo da operação: ")
-                            op = str(input(f"{sm} "))
-                            if verification_mat_operation(op) == True:
-                                n2 = float(input(f"{sm} {op} "))
-                                if verification_digit(n2) == True:
-                                    sm = f"{sm}{op}{n2}"
-                                    sm = eval(sm)
-                                    print(sm)
-                                    dj = int(input("1 == continuar \n2 == Sair\n"))
-                                    if verification_digit(dj) == True:
-                                        if(dj == 1):
-                                            continue
-                                        elif(dj == 2):
-                                            continue
-                                        elif(dj == 3):
-                                            break
-                                    else:
-                                        print("Escolha uma opção numerica !")
-                                        os.system("cls")
-                                else:
-                                    print("Digite um numero !")
-                                    os.system("cls")
-                            else:
-                                print("Digite um simbolo de operação matematica na proxima !")
-                                os.system("cls")
-                    elif(dj == 2):
-                        continue
-                    elif(dj == 3):
-                        break
-                else:
-                    print("Escolha uma opção numerica !")
-                    os.system("cls")
-            else:
-                print("Digite um numero !")
-                os.system("cls")
-        else:
-            print("Digite um simbolo de operação matematica na proxima !")
-            os.system("cls")
-    else:
-        print("Digite um numero !")
-        os.system("cls")
 
+while True:
+    print(showresul(n))
+    print(menunum())
+    n = input(" ")
+    n = n
+    os.system("cls")
+    print(showresul(n))
+    print(menuope())
+    op = str(input(" ")) 
+    resultado = f"{n} {op}"
+    os.system("cls")
+    print(showresul(resultado))
+    print(menunum())
+    n2 = input(" ")
+    resueval = eval(resultado+str(n2))
+    resultado = f"{n} {op} {n2} == {resueval}"
+    os.system("cls")
+    print(showresul(resultado))
+    print(menuope())
+    op = str(input(" "))
+    while op != "=":
+        os.system("cls")
+        acu = resueval
+        resultado = f"{acu} {op}"
+        print(showresul(resultado))
+        print(menunum())
+        n2 = input(" ")
+        os.system("cls")
+        resueval = eval(resultado+str(n2))
+        resultado = f"{acu} {op} {n2} == {resueval}"
+        print(showresul(resultado))
+        print(menuope())
+        op = str(input(" "))
+        time.sleep(3)
+    os.system("cls")
+    print(showresul(resultado))
+    print("---------------------------------------------------------------------------")
+    break
+    
     
