3faf75b0d2bfe9a0c7f3c550814e824a29d888a3
NecoMatheus <matheus770@users.noreply.github.com>
2022-07-06 20:05:29 -0300

    Refatoração de codigo, criando validações

--- main.py
+++ main.py
@@ -1,57 +1,43 @@
 import os, time
-def menunum():
-    return("""
-          1 | 2 | 3 | 4 | 5 |
-          6 | 7 | 8 | 9 | 0 |
-          + | - | / | * | = |
-          """)
-def menuope():
-        return("""
-          + | - | / | * | = |
-          """)
-def showresul(x):
-    str(x)
-    return(f"{x}")
-
+from funcs import *
 
 n = 0
 
 while True:
-    print(showresul(n))
-    print(menunum())
-    n = input(" ")
-    n = n
+    showresul(n)
+    menunum()
+    n = inputnum()
     os.system("cls")
-    print(showresul(n))
-    print(menuope())
-    op = str(input(" ")) 
+    showresul(n)
+    menuope()
+    op = inputsimbol()
     resultado = f"{n} {op}"
     os.system("cls")
-    print(showresul(resultado))
-    print(menunum())
-    n2 = input(" ")
+    showresul(resultado)
+    menunum()
+    n2 = inputnum()
     resueval = eval(resultado+str(n2))
     resultado = f"{n} {op} {n2} == {resueval}"
     os.system("cls")
-    print(showresul(resultado))
-    print(menuope())
-    op = str(input(" "))
+    showresul(resultado)
+    menuope()
+    op = inputsimbol()
     while op != "=":
         os.system("cls")
         acu = resueval
         resultado = f"{acu} {op}"
-        print(showresul(resultado))
-        print(menunum())
-        n2 = input(" ")
+        showresul(resultado)
+        menunum()
+        n2 = inputnum()
         os.system("cls")
         resueval = eval(resultado+str(n2))
         resultado = f"{acu} {op} {n2} == {resueval}"
-        print(showresul(resultado))
-        print(menuope())
-        op = str(input(" "))
+        showresul(resultado)
+        menuope()
+        op = inputsimbol()
         time.sleep(3)
     os.system("cls")
-    print(showresul(resultado))
+    showresul(resultado)
     print("---------------------------------------------------------------------------")
     break
     
